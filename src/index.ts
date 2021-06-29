06508533d4049b9caa72e76b3fb255dfe4403423
Amir Bilu <amirbilu@codota.com>
2021-06-29 11:32:05 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import fs from "fs";
+import path from "path";
+import walk from "walkdir";
+
+function main() {
+  const domains = getAllDomains(path.resolve(__dirname, "../lib/domains"));
+  console.log(JSON.stringify(domains, null, 2));
+}
+
+function getAllDomains(rootDir: string): { [key: string]: boolean } {
+  const domains: { [key: string]: boolean } = {};
+  walk.sync(rootDir, (path) => {
+    if (isDomainFile(path)) {
+      const domain = parseSingleDomain(path);
+      domains[domain] = true;
+    }
+  });
+  return domains;
+}
+function parseSingleDomain(path: string): string {
+  // src/assets/.../domains/il/co/example.txt => example.co.il
+  const trimmedPath = path.replace(/\.txt$/, "");
+  const relativePath = trimmedPath.replace(/^.*domains\//, "");
+  return relativePath.split("/").reverse().join(".");
+}
+
+function isDomainFile(path: string) {
+  return path.endsWith(".txt");
+}
+
+main();
