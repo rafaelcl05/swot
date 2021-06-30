c165d78e0b85d5dd930588cd88de51660d1a288b
yonip23 <67855609+yonip23@users.noreply.github.com>
2021-06-30 12:11:52 +0300

    add script to support open domain pr automatically. (#4)

---
+++ src/scripts/openDomainPR.ts
@@ -0,0 +1,36 @@
+import fs from "fs";
+import path from "path";
+
+const basePath = path.resolve(__dirname, "../../lib/domains");
+console.log(basePath);
+function main() {
+  const args = process.argv.slice(2);
+  if (args.length !== 2) {
+    console.error('Usage: node [this script] [domain] "[domain description]" ');
+    process.exit(1);
+  }
+
+  const [domain, description] = args;
+
+  if (!/^[a-zA-Z.]*$/.test(domain)) {
+    console.error(
+      "Domain must be in the form of (xxx.)* [means only letters followed by dots]"
+    );
+    process.exit(1);
+  }
+
+  const dirs = domain.split(".").reverse();
+  const [fileName] = dirs.splice(-1, 1);
+
+  const targetPath = path.resolve(basePath, ...dirs);
+
+  if (!fs.existsSync(targetPath)) {
+    fs.mkdirSync(targetPath, { recursive: true });
+  }
+
+  const fullFilePath = path.resolve(targetPath, `${fileName}.txt`);
+  console.log("Creating new file: ", fullFilePath);
+  fs.writeFileSync(fullFilePath, description);
+}
+
+main();
